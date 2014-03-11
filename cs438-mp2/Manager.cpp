a83141d97398d5ddb82dc4725e22ec43d65488e1
hzhang49 <zzh915@gmail.com>
2014-03-10 23:56:15 -0500

    open file

--- cs438-mp2/Manager.cpp
+++ cs438-mp2/Manager.cpp
@@ -3,9 +3,36 @@
 
 Manager::Manager()
 {
+	readTopologFile();
+	readMessageFile();
+
+
 }
 
 
 Manager::~Manager()
 {
+}
+
+Manager::readTopologFile()
+{
+	FILE * topology_file;
+
+	topology_file = fopen ("topology.txt","r");
+	if(topology_file == NULL){
+		printf("Error opening topology file");
+		return -1;
+	}
+}
+
+Manager::readMessageFile()
+{
+	FILE * message_file;
+
+	message_file = fopen ("message.txt","r");
+	if(message_file == NULL){
+		printf("Error opening message file");
+		return -1;
+	}
+
 }
