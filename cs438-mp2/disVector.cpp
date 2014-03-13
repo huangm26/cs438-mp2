2e06a691d75707ef15817b0307f796998831c6ec
huangm26 <huangm26@gmail.com>
2014-03-13 01:19:47 -0500

    conmitnaoeng

---
+++ cs438-mp2/disVector.cpp
@@ -0,0 +1,9 @@
+//
+//  disVector.cpp
+//  
+//
+//  Created by Meng Huang on 14-3-13.
+//
+//
+
+#include "disVector.h"
---
+++ cs438-mp2/disVector.h
@@ -0,0 +1,14 @@
+//
+//  disVector.h
+//  
+//
+//  Created by Meng Huang on 14-3-13.
+//
+//
+
+#ifndef ____disVector__
+#define ____disVector__
+
+#include <iostream>
+
+#endif /* defined(____disVector__) */
